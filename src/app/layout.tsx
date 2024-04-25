0c2527e13e3edc577e668bc811bf02c76e20a9c2
Val <val.samonte@gmail.com>
2024-04-26 02:26:52 +0800

    Change navbar design

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -41,25 +41,27 @@ export default function RootLayout({
       <meta name='theme-color' content='#ffffff' />
       <body className={inter.className}>
         <main className='relative flex min-h-screen flex-col px-4 pb-4 xl:p-16 gap-4 xl:gap-12 items-center'>
-          <nav className='sticky top-0 flex min-h-16 w-full items-center justify-between from-white to-white xl:to-white/0 bg-gradient-to-b z-20'>
-            <Link
-              href='https://www.facebook.com/groups/solanaphilippines'
-              target='_blank'
-              rel='noreferrer noopener'
-              className='flex items-center gap-4'
-            >
-              <Image
-                src={'logo.svg'}
-                alt='Solana PH'
-                width={200}
-                height={200}
-                className='h-14 w-14'
-              />
-              <h1 className='text-gray-600'>Solana Philippines</h1>
-            </Link>
-            <ul className='flex items-center gap-6 xl:gap-12 flex-auto justify-end'>
-              <li>Directory</li>
-            </ul>
+          <nav className='sticky top-0 flex min-h-16 w-full bg-white/75 backdrop-blur z-20'>
+            <div className='flex items-center justify-between w-full max-w-7xl mx-auto'>
+              <Link
+                href='https://www.facebook.com/groups/solanaphilippines'
+                target='_blank'
+                rel='noreferrer noopener'
+                className='flex items-center gap-4'
+              >
+                <Image
+                  src={'logo.svg'}
+                  alt='Solana PH'
+                  width={200}
+                  height={200}
+                  className='h-14 w-14'
+                />
+                <h1 className='text-gray-600'>Solana Philippines</h1>
+              </Link>
+              <ul className='flex items-center gap-6 xl:gap-12 flex-auto justify-end'>
+                <li>Directory</li>
+              </ul>
+            </div>
           </nav>
           {children}
         </main>
